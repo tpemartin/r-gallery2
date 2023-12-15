173fee24bbf254e82b45fdecd6610c6e9dece6e7
Martin老師 <tpemartin@gmail.com>
2023-12-16 05:15:47 +0800

    publish

--- vite.config.js
+++ vite.config.js
@@ -20,8 +20,7 @@ export default defineConfig({
         week12: resolve(root, 'week12/index.html'),
         week13: resolve(root, 'week13/index.html'),
         week14: resolve(root, 'week14/index.html'),
-        week15: resolve(root, 'week15/index.html'),
-        fetch: resolve(root, 'fetch/index.html')
+        week15: resolve(root, 'week15/index.html')
       }
     }
   }
